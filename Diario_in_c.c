1f41ab2ac36085f439eede8486e9e12da670052b
Arianna Girardi <iaiagira16@gmail.com>
2015-04-17 10:36:12 +0200

    creato due funz

--- Diario_in_c.c
+++ Diario_in_c.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int inserisci(int,int);
+void rimuovi(int,int);
+
 int main(){
 printf("Ciao\n");
 return 0;
 }
+
+int inserisci(int n1,int n2){
+
+return 1;
+}
+	
+void rimuovi(int n1,int n2){
+	
+}
+
